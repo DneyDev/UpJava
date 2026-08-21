6df945ec95b1133075ac9e5e276b7029b5877275
DneyDev <contdneydev@gmail.com>
2026-08-21 14:02:13 -0300

    feat(fase02): implementa Produto com construtor, this e setters validados

--- Main.java
+++ Main.java
@@ -1,30 +1,17 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        //Exercício para treinamento de implementar Validador de senha com metodos estaticos e manipulacao de String
+        //Exercício para treinamento de implementar classe Produto com construtor, this e setters validados
+        Produto produto = new Produto("Teclado Mecânico", 250.0, 5);
+        produto.exibirInfo();
 
-        try(Scanner leitor = new Scanner(System.in)){
-            System.out.print("Digite uma senha: ");
-            String senha = leitor.nextLine();
+        produto.venderUnidade();
+        produto.venderUnidade();
+        produto.exibirInfo();
 
-            System.out.println("Força da senha: " + ValidadorDeSenha.avaliarForca(senha));//aqui fazemos a chamada da Classe do validador e usamos o método avaliarForca, o mesmo ocorre nas operações abaixo
-            System.out.println(
-                "Tamanho mínimo: " +
-                ValidadorDeSenha.temTamanhoMinimo(senha)
-            );
-            System.out.println(
-                "Possui número: " +
-                ValidadorDeSenha.temNumero(senha)
-            );
-            System.out.println(
-                "Possui letra maiúscula: " +
-                ValidadorDeSenha.temLetraMaiuscula(senha)
-            );
-            System.out.println(
-                "Possui caractere especial: " +
-                ValidadorDeSenha.temCharEspecial(senha)
-            );
+        try {
+            produto.setPrice(-10);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Erro: " + e.getMessage());
         }
     }
 }
